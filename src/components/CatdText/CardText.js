842f72053b8cf05ac9f261974cb88a61c2a7a384
Augustin Anders <augustinanders@gmail.com>
2023-03-10 09:08:44 +0100

    add componentes

---
+++ src/components/CatdText/CardText.js
@@ -0,0 +1,10 @@
+export default function CardText() {
+  return (
+    <p className="entries-card__text">
+      Amet sunt tempor culpa eiusmod sunt magna proident eiusmod enim.
+      Incididunt cillum veniam sit consequat nostrud sunt. Cupidatat excepteur
+      sunt ex in amet veniam quis. Quis aliqua culpa sint incididunt id velit
+      aliqua fugiat pariatur.
+    </p>
+  );
+}
